5797a3e46c4d7e7dc9ea9b45c0f35e0d17084b14
yasiruranathunga <yasirusr99@gmail.com>
2023-05-17 11:05:21 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,21 @@ import { FinancialManagement } from "./components/FinancialManagement";
 import { Delinquency } from "./components/Delinquency";
 import { Retention } from "./components/Retention";
 import { LeaseFileAudit } from "./components/LeaseFileAudit";
+import {Routes , Route} from 'react-router-dom';
+import Testt from "./components/Testt";
+import { ExampleTree } from "./components/OrganizationalChart";
+
 
 function App() {
   return (
-    <div className="container">
+
+    <Routes>
+       <Route path='/' element={<Testt/>} />
+       <Route path='/Organization_Chart' element={<ExampleTree/>} />
+      <Route path='/dash' element={<div className="container">
       <div className="py-8 md:py-[74px] gap-[18px] h-screen grid lg:grid-rows-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
-        <Occupancy />
+        
+        <Occupancy />  
         <Delinquency />
         <Retention />
         <FinancialManagement />
@@ -18,7 +27,15 @@ function App() {
         <ReputationManagement />
         <Marketing />
       </div>
-    </div>
+    </div>} />
+    
+
+
+
+
+    </Routes>
+
+
   );
 }
 
